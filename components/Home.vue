3e7570a43792a664ba1396b5620fccec33d19467
Nikxphreaker <nikx449@gmail.com>
2024-05-31 21:48:21 +0700

    update typing effect for job

--- components/Home.vue
+++ components/Home.vue
@@ -7,7 +7,49 @@ const onLoad = (container: Container) => {
   container.pause()
   setTimeout(() => container.play(), 2000)
 }
-const me = useState('me', () => this == null ? true : !this);
+const showMe = useState('showMe', () => this == null ? true : !this);
+function toggleShow(ev: Event) { showMe.value = !showMe.value; }
+// const myRole = ["Mobile Developer", "Web Developer", "Game Developer"];
+// const myRoleFinal = useState('myRoleFinal', () => '');
+// const i = useState('myRole', () => 0);
+
+const myRole = useState('typingText', () => '');
+const typingWords = ["Mobile Developer", "Web Developer", "Game Developer"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typingWriter = () => {
+  const currentWord = typingWords[wordIndex];
+
+  if (isDeleting && charIndex > 0) {
+    myRole.value = currentWord.substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(typingWriter, 100);
+  } else if (!isDeleting && charIndex < currentWord.length) {
+    myRole.value = currentWord.substring(0, charIndex + 1);
+    charIndex++;
+    setTimeout(typingWriter, 100);
+  } else {
+    isDeleting = !isDeleting
+
+    if (!isDeleting) {
+      wordIndex = (wordIndex + 1) % typingWords.length;
+      charIndex = 0;
+    }
+    setTimeout(typingWriter, 2000);
+  }
+}
+
+typingWriter()
+// setTimeout(() => { i.value++ }, 9000);
+onMounted(() => {
+  // setInterval(() => { i.value == (myRole.length - 1) ? i.value = 0 : i.value++ }, 6000); console
+  //   .log(i.value)
+  // setInterval(() => { myRoleFinal.value = myRole[i.value].substring(0, 2) }, 1000);
+});
+
+onUpdated(() => {
+});
 // const onClickMe = () => { me = false }
 // defineProps({ onClickMe })
 </script>
@@ -15,15 +57,27 @@ const me = useState('me', () => this == null ? true : !this);
 <template>
   <div class="bg-primary">
     <Header />
-    <div class="absolute flex flex-col items-center justify-center bg-transparent w-full h-screen">
-      <img
-        class="w-60 md:w-96 rounded-full shadow-md shadow-green-400 hover:shadow-xl hover:shadow-green-500 hover:cursor-pointer"
-        src="~/assets/image/nikx.jpg" />
-      <div>
-        <h2 class="text-white" :hidden="me">Niko Prayoga Wiratama</h2>
+    <div class="absolute flex flex-col justify-center bg-transparent w-full h-screen">
+      <div class="ms-[50%] self-start font-mono">
+        <div class="-mt-12 transition ease-in-out flex flex-col"
+          :class="showMe ? 'translate-x-[13rem] text-5xl opacity-100' : 'opacity-0'">
+          <span class="text-white z-10" :hidden="false">I'm Niko, a</span>
+          <span
+            class="absolute mt-12 animate-blink overflow-hidden whitespace-nowrap border-r-4 border-r-green-500 text-green-500 z-10"
+            :hidden="false">{{ myRole }}</span>
+        </div>
+        <!-- <div class="transition ease-in-out" :class="showMe ? 'translate-x-96 text-5xl opacity-100' : 'opacity-0'">
+          <a class="text-white z-10" :hidden="false">Mobile Developer</a>
+        </div>
+        <div class="transition ease-in-out" :class="showMe ? 'translate-x-96 text-5xl opacity-100' : 'opacity-0'">
+          <a class="text-white z-10" :hidden="false">Mobile Developer</a>
+        </div> -->
       </div>
+      <img
+        class="absolute self-center z-30 w-60 md:w-96 rounded-full shadow-md shadow-green-400 hover:shadow-xl hover:shadow-green-500 hover:cursor-pointer"
+        src="~/assets/image/nikx.jpg" @click="toggleShow" draggable="false" />
     </div>
-    <NuxtParticles id="tsparticles" :url=urlParticleOptions @load="onLoad" class="w-full h-screen">
+    <NuxtParticles id="tsparticles" :url=urlParticleOptions @load="onLoad" class="w-full relative h-screen">
     </NuxtParticles>
   </div>
   <!-- <div class="image"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,30 @@ export default <Partial<Config>>{
         extend: {
             colors: {
                 primary: "#22201F"
+            },
+            keyframes: {
+                typing: {
+                    "0%": {
+                        width: "0%",
+                        visibility: "hidden"
+                    },
+                    "100%": {
+                        width: "100%"
+                    }
+                },
+                blink: {
+                    "50%": {
+                        borderColor: "transparent"
+                    },
+                    "100%": {
+                        borderColor: "#22201F"
+                    }
+                }
+            },
+            animation: {
+                typing: "typing 2s steps(16) normal, typing 1s steps(16) 5s reverse, blink .7s infinite",
+                blink: "blink .7s infinite",
             }
-        }
+        },
     }
 }
