cdfcce786997732993410a387659fb18f499d3ad
Nikxphreaker <nikx449@gmail.com>
2024-06-03 22:09:52 +0700

    add resume download

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="min-h-screen bg-white" id="about">
-    <div class="text-center py-20 flex flex-col">
+    <div class="text-center py-20 flex flex-col items-center">
       <fa icon="fa-user" size="3x" color="green" />
       <span class="text-3xl font-bold font-mono max-[450px]:text-2xl">About <a class="text-green-700">Me</a></span>
       <img src="~/assets/image/nikx.jpg"
@@ -20,6 +20,13 @@
         in ECS Alibaba Cloud, Hostinger, Plesk, etc.
         Always practice and produce several projects which I saved on github.
       </p>
+      <a class="w-56 mt-9 bg-black hover:bg-green-500 text-white rounded-full py-3 px-5 text-[17px] ms-5 max-[1024px]:ms-0 hover:cursor-pointer [&>*]:hover:animate-bounce hover:shadow-rounded hover:shadow-green-500"
+        href="https://drive.google.com/file/d/1HcG801wLwRdcFRxED8mTM6F17slQ0bV_/view" target="_blank">
+        <span>Download Resume</span>
+        <fa-layers class="ms-3 fa-1x">
+          <fa icon="fa-square-caret-down"></fa>
+        </fa-layers>
+      </a>
     </div>
   </div>
 </template>
